4c2d5c0aa0ed863a5db48c15a26ada2d2b252afa
Nitish jha <nitishjha6519@gmail.com>
2023-02-09 22:36:20 +0530

    assignment link added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const tasksRoute = require("./routes/tasks");
-
+// https://docs.google.com/document/d/1t9iUeN1IqQoQ8msxCWSFryKPQUnHRtGaKZi1qbF59wQ/edit#
 mongoose.connect("mongodb://127.0.0.1/prt2", (err) => {
   if (!err) {
     console.log("connected to DB");
